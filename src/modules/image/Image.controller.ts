9892308d73fe7c8e983febb77e6de3aa6a1a15a8
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-08-16 14:43:34 -0500

    import errors correction

--- src/modules/image/Image.controller.ts
+++ src/modules/image/Image.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { uploadFile } from "../../utils/aws";
-import { asyncHandler } from "@/core";
+import { asyncHandler } from "../../core";
 
 export const uploadImage = asyncHandler(async (req: Request, res: Response) => {
   if (!req.file) {
--- src/modules/image/Image.routes.ts
+++ src/modules/image/Image.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 import { uploadImage } from "./Image.controller";
-import { authenticateToken } from "@/middelwares/auth.middleware";
+import { authenticateToken } from "../../middelwares/auth.middleware";
 
 const router = Router();
 const upload = multer({ storage: multer.memoryStorage() });
